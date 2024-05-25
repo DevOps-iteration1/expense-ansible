cd525687123a25789a29b958ae9512d425cb2cf8
Vivek <vivekanandareddy7677@gmail.com>
2024-05-25 18:55:01 -0500

    Updating common role

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,5 @@
 component=$1
-sudo pip3 install ansible hvac
+sudo pip3 install ansible hvac -y
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
 ansible-playbook -i $component-$env.vdevops.cloud, -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json' -e '@~/app.json'
 
