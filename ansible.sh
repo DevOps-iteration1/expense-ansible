46358b44d24374587086274154c30f9a2b933666
Vivek <vivekanandareddy7677@gmail.com>
2024-05-26 23:02:10 -0500

    Automating logs management

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
- pip3.11 install ansible hvac
+pip3.11 install ansible hvac
 component=$1
 ansible-playbook get-secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
 ansible-playbook -i $component-$env.vdevops.cloud, -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json' -e '@~/app.json'
